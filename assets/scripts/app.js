1f555edc04044af91db17f7f4151daa11bda291f
Guillermo Moratorio <moratorio.guillermo@gmail.com>
2016-11-14 12:56:04 -0700

    placed nav bar - starting on jumbotron

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
 
-    // $.getJSON("https://sheets.googleapis.com/v4/spreadsheets/1ouyI7JWT2agLynYywzFkqnOzID8u9Q5FeSR1ZhPz1Rk?includeGridData=true&key=AIzaSyAW02DgWURAhAMVrDiDS13Rg7Nv-TG2e7Y")
-    //feed options - list or cells
     $.getJSON("http://spreadsheets.google.com/feeds/cells/1ouyI7JWT2agLynYywzFkqnOzID8u9Q5FeSR1ZhPz1Rk/od6/public/basic?alt=json-in-script&callback=?")
         .done(function(data) {
 
@@ -12,43 +10,38 @@ $(document).ready(function() {
             console.log(data);
         });
 
-    function populatePage(data) {
-        // console.log(data.feed.entry);
-        var title = data.feed.entry[6].content.$t;
-        var image = data.feed.entry[7].content.$t;
-        console.log(image);
-
-
-        $main = $('main');
-        $title = $("<h2>" + title + "</h2>");
-        $image = $("<img src='" + image + "'/>");
-
-
-        // console.log($main);
-        $main.append($image);
-        $main.append($title);
-
-        for (var i = 9; i < 12; i++) {
-            var text = data.feed.entry[i].content.$t;
-            $p = $("<p>" + text + "</p>");
-            $main.append($p);
-        }
-
-        // console.log(title + ": " + text);
-    }
-
-
-    // // use your spreadsheet id here
-    // var SPREADSHEET_ID = '1ouyI7JWT2agLynYywzFkqnOzID8u9Q5FeSR1ZhPz1Rk'
-    // $.googleSheetToJSON(SPREADSHEET_ID)
-    //     .done(function(rows){
-    //         // each row is a row of data from the spreadsheet
-    //         console.log(rows[0]);
-    //     })
-    //     .fail(function(err){
-    //         console.log('error!', err);
-    //     });
-
-
+        $(window).scroll(function() {
+          var $brand = $(".navbar-brand")[0];
+          var $navRight = $(".navbar-right")[0];
+          if ($(document).scrollTop() > 50) {
+            $($brand).addClass('shrink');
+            $($navRight).addClass('shrink');
+          } else {
+            $($brand).removeClass('shrink');
+            $($navRight).removeClass('shrink');
+          }
+        });
 
 });
+
+function populatePage(data) {
+    // var title = data.feed.entry[6].content.$t;
+    // var image = data.feed.entry[7].content.$t;
+    // console.log(image);
+    //
+    //
+    // $main = $('main');
+    // $title = $("<h2>" + title + "</h2>");
+    // $image = $("<img src='" + image + "'/>");
+    //
+    //
+    // $main.append($image);
+    // $main.append($title);
+    //
+    // for (var i = 9; i < 12; i++) {
+    //     var text = data.feed.entry[i].content.$t;
+    //     $p = $("<p>" + text + "</p>");
+    //     $main.append($p);
+    // }
+
+}
